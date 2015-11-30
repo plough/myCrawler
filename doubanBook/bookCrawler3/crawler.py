f900cdfa038b7b7c046e0a27b8ea3b7a00c21b91
plough <tzgdaydayup@gmail.com>
2015-11-30 23:15:49 +0800

    伪装成浏览器，避免403错误

--- doubanBook/bookCrawler3/crawler.py
+++ doubanBook/bookCrawler3/crawler.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
 # 使用requests和BeautifulSoup模块，爬取豆瓣的图书信息。先爬标签，再爬每个标签下的书籍信息。
 # 用子线程来爬取图片信息和书籍详情
-
-# 把str编码由ascii改为utf8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 import requests
 from bs4 import BeautifulSoup
 import MySQLdb
@@ -16,6 +9,11 @@ import time
 import os
 from multiprocessing.dummy import Pool as ThreadPool
 import random
+# 把str编码由ascii改为utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 class BookCrawler:
     def __init__(self):
@@ -24,13 +22,13 @@ class BookCrawler:
         self.f_content = ''
         # 伪装成浏览器，防止403错误
         self.headers = [
-                {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},\
-                {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},\
-                {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},\
-                {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},
-                {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0'},
-                {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'}
-                ]
+            {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},
+            {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},
+            {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},
+            {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},
+            {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0'},
+            {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'}
+        ]
         # 用于爬取网页前，随机获取一个header
         self.randHeaders = lambda : self.headers[random.randint(0, len(self.headers)-1)]
 
--- doubanBook/bookSpiderXPath.py
+++ doubanBook/bookSpiderXPath.py
@@ -2,12 +2,22 @@
 # encoding: utf-8
 import time
 import requests
+import random
 from lxml import etree
 # 把str编码由ascii改为utf8
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
+headers = [
+    {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},
+    {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},
+    {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},
+    {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0'},
+    {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'}
+]
 
 def main():
     tags = ['哲学', '计算机', '心理学', '生活', '数学']
@@ -29,7 +39,9 @@ def bookSpider(bookTag):
     result += divide + '\t' * 4 + bookTag + '：' + divide
 
     url = "http://www.douban.com/tag/%s/book" % bookTag
-    html = requests.get(url).content
+    global headers
+    html = requests.get(url, headers=random.choice(headers)).content
+    #  print html
     tree = etree.HTML(html.decode('utf-8'))
     books = tree.xpath("//dl/dd")
 
@@ -55,3 +67,4 @@ def bookSpider(bookTag):
 
 if __name__ == "__main__":
     main()
+    #  bookSpider('哲学')
